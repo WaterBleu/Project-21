e050e8a0be571a502416dcb7a6ce0d68bc1913d1
WaterBleu <jeff7746@hotmail.com>
2015-08-28 17:00:43 -0700

    Include pod files

---
+++ Pods/Target Support Files/AAShareBubbles/AAShareBubbles-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "AAShareBubbles.h"
+
+FOUNDATION_EXPORT double AAShareBubblesVersionNumber;
+FOUNDATION_EXPORT const unsigned char AAShareBubblesVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Project21/Pods-Project21-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Project21VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Project21VersionString[];
+
